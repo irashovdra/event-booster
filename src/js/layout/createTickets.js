70e0d131fd6961ee1c84c732a61c86c1a6aa6028
irashovdra <109846925+irashovdra@users.noreply.github.com>
2024-11-30 11:09:10 -0600

    added header-bg for every device & outline for cards

--- src/js/layout/createTickets.js
+++ src/js/layout/createTickets.js
@@ -7,6 +7,7 @@ export const createTicket = ({
   return `<li class="ticket">
             <div class="ticket__photo-wrapper">
               <img src="${eventImage}" alt="${eventName}" class="ticket__photo">
+              <div class="ticket__photo-border"></div>
             </div>
             <div class="ticket__details">
               <h3 class="ticket__title">${eventName}</h3>
